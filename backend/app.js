18d534e984a90ee50128ea525ab4300fe0c5a916
qwertyNippon <orlandohenry100@gmail.com>
2023-08-08 23:19:49 -0400

    added more code

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
+const express = require('express')
+const cors = require('cors')
 
+const app = express()
+require('dotenv').config()
 
+const PORT = process.env.PORT
 
+//middlewares
+app.use(express.json())
+app.use(cors({
+
+}))
 const server = () => {
-
+    console.log('works'. PORT)
 }
 
 server()
