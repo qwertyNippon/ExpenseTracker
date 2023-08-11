e1f3a27962b0a048f1eca9cd49369542be97fd59
qwertyNippon <orlandohenry100@gmail.com>
2023-08-10 22:05:26 -0400

    update

---
+++ backend/models/IncomeModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+
+const IncomeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    amount: {
+        type: Number,
+        required: true,
+        maxLength: 20,
+        trim: true
+    },
+    type: {
+        type: String,
+        default: "income"
+    },
+    date: {
+        type: Date,
+        required: true,
+        trim: true
+    },
+    category: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 20,
+        trim: true
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model('Income', IncomeSchema)
+
+
+// trim: true,
+// It's basically there to ensure the strings you 
+// save through the schema are properly trimmed. 
+// If you add { type: String, trim: true } to a field 
+// in your schema, then trying to save strings like 
+// "  hello", or "hello ", or "  hello ", would end up 
+// being saved as "hello" in Mongo - i.e. white spaces 
+// will be removed from both sides of the string.
