1f7dc6d5aafff2687a0f6ef79ea110afd30b50cf
qwertyNippon <orlandohenry100@gmail.com>
2023-08-13 00:36:11 -0400

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
 import styled from 'styled-components';
+import bg from '../src/img/bg.png'
 
 function App() {
   return (
-    <AppStyled className="App">
-
+    <AppStyled bg = {bg} className="App">
+      <main>
+        
+      </main>
     </AppStyled>
   );
 }
+
 const AppStyled = styled.div`
   height: 100vh;
-  
+  background-image: url(${props => props.bg});
+  position: relaive;
 `;
 
 export default App;
