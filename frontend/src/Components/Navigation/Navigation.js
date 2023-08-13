5566baec377e9c5f6b8d69e6a43e6221d77dd5d9
qwertyNippon <orlandohenry100@gmail.com>
2023-08-13 12:52:30 -0400

    update

--- frontend/src/Components/Navigation/Navigation.js
+++ frontend/src/Components/Navigation/Navigation.js
@@ -1,9 +1,21 @@
 import React from "react";
 import styled from 'styled-components'
+import avatar from '../../img/avatar.png'
 
 function Navigation() {
     return (
-        <div>Navi</div>
+        <NavStyled>
+            <div className="user-con">
+                <img src = {avatar} alt = '' />
+                <div className="text">
+                    <h2>Yuri</h2>
+                    <p>Our Money</p>
+                </div>
+            </div>
+            <div className="menu-items">
+
+            </div>
+        </NavStyled>
     )
 }
 
