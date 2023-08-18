93c814782e856531437c3b74b98eefcf2b4959c9
qwertyNippon <orlandohenry100@gmail.com>
2023-08-18 12:48:24 -0400

    update

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -3,7 +3,7 @@ const ExpenseSchema = require("../models/ExpenseModel")
 exports.addExpense = async (req, res) => {
     const {title, amount, category, description, date} = req.body
 
-    const expense = ExpenseSchema({
+    const income = ExpenseSchema({
         title,
         amount,
         category,
@@ -20,7 +20,7 @@ exports.addExpense = async (req, res) => {
         if(amount <= 0 || !amount === 'number') {
             return res.status(400).json({message: "All fields are required"})
         }
-        await expense.save()
+        await income.save()
         res.status(200).json({message : 'Expense added'})
     } catch (error) {
         res.status(500).json({message : 'Server error'})
@@ -29,8 +29,8 @@ exports.addExpense = async (req, res) => {
 
 exports.getExpenses = async (req, res) => {
     try {
-        const expenses = await ExpenseSchema.find().sort({createdAt: -1})
-        res.status(200).json(expenses)
+        const incomes = await ExpenseSchema.find().sort({createdAt: -1})
+        res.status(200).json(incomes)
     } catch (error) {
         res.status(500).json({message : 'Server error'})       
     }
@@ -39,10 +39,10 @@ exports.getExpenses = async (req, res) => {
     exports.deleteExpense = async (req, res) => {
         const {id} = req.params;
         ExpenseSchema.findByIdAndDelete(id)
-            .then((expense) => {
+            .then((income) => {
                 res.status(200).json({message : 'Expense deleted'})       
             })
-            .catch((error) => {
+            .catch((err) => {
                 res.status(500).json({message : 'Server error'})       
             })
 }
